e6bd30eea6001c1b2c1e86920b12b5c8436411bc
Gorshipisk <107960553+Gorshipiskp@users.noreply.github.com>
2023-03-24 19:51:52 +0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -1,3 +1,13 @@
 import sqlite3
-import config
 import os
+import random
+import time
+import config
+import errors
+from codes import *
+from actions import *
+from numba import jit
+from multiprocessing import Process
+
+
+lang = getattr(__import__("languages", fromlist=[config.language]), config.language)
